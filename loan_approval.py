553860096db505805a705b80de5e54b25a8779f9
CHetanthakur2000 <csikarwar253@gmail.com>
2026-07-07 11:06:58 +0530

    Add loan approval program using logical operators

---
+++ loan_approval.py
@@ -0,0 +1,8 @@
+#user input
+age = int(input("Enter your age: "))
+salary = int(input("Enter your salary: "))
+
+if age >= 18 and salary >= 15000:
+	print("Loan Approved")
+else:
+	print("Loan Rejected")
